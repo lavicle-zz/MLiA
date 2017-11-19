7f3f8f406705e71e1b3b24d7a5a35efdb30dfc51
lavicle <29335639+lavicle@users.noreply.github.com>
2017-11-19 21:47:18 +0800

    add b in the calculation of error_cache

--- SVM/fullSMO.py
+++ SVM/fullSMO.py
@@ -24,19 +24,21 @@ class OptStructure:
         self.y_matrix = y_matrix.T
         self.tole = tole
         self.m = y_matrix.shape[1]
-        self.alphas = np.matrix(np.zeros((self.m, 1)))
+        self.alphas = np.asmatrix(np.zeros((self.m, 1)))
         self.b = 0
-        self.e_cache = np.matrix(np.zeros((self.m, 2)))  # why the dimension should be m*2 error Cache
+        self.e_cache = np.asmatrix(np.zeros((self.m, 2)))  # why the dimension should be m*2 error Cache
 
-def take_steps(os, i, c):
+
+def take_steps(os, i):
     # input(what elements of os)
     # the output: update alpha_i, alpha_j and b
     ei = calc_e(os, i)
     kkt_or_not = (os.alphas[i] > 0 and os.y_matrix[i] * ei > os.tole) \
-                 or (os.alphas[i] < c and os.y_matrix[i] * ei < -os.tole) # why >> and <<
+                 or (os.alphas[i] < os.c and os.y_matrix[i] * ei < -os.tole) # why >> and <<
     # select the i which violates the KKT condition
     if kkt_or_not:
-        j, ej = select_j(os, i, ei)
+        # j, ej = select_j(os, i, ei)
+        j, ej = select_j_random(os, i)
         kii = os.x_matrix[i, :] * (os.x_matrix[i, :]).T
         kij = os.x_matrix[i, :] * (os.x_matrix[j, :]).T
         kjj = os.x_matrix[j, :] * (os.x_matrix[j, :]).T
@@ -46,19 +48,21 @@ def take_steps(os, i, c):
             return 0
         alpha_i_old = os.alphas[i].copy()
         alpha_j_old = os.alphas[j].copy()
-        delta_alpha_j = os.y_matrix[j] / eta * (ej - ei)
+        delta_alpha_j = os.y_matrix[j] * (ej - ei) / eta
         alpha_j_new = alpha_j_old + delta_alpha_j
         # calHL
         if os.y_matrix[i] == os.y_matrix[j]:
-            h = min(alpha_j_old + alpha_i_old, c)
-            l = max(0, alpha_j_old + alpha_i_old - c)
+            h = min(alpha_j_old + alpha_i_old, os.c)
+            l = max(0, alpha_j_old + alpha_i_old - os.c)
         else:
-            h = min(c, c + alpha_j_old - alpha_i_old)
+            h = min(os.c, os.c + alpha_j_old - alpha_i_old)
             l = max(0, alpha_j_old - alpha_i_old)
         # clip_alpha_j
         if h == l:
-            print('h==l')
+            print('h==l， %d， %d', (h, l))
             return 0
+        else:
+            print('not h==l')
         if alpha_j_new < l:
             alpha_j_new = l
         elif alpha_j_new > h:
@@ -79,9 +83,9 @@ def take_steps(os, i, c):
         bj = os.b - ej \
              - delta_alpha_i * os.y_matrix[i] * (os.x_matrix[i, :] * (os.x_matrix[j, :]).T) \
              - delta_alpha_j * os.y_matrix[j] * (os.x_matrix[j, :] * (os.x_matrix[j, :]).T)
-        if 0 < os.alphas[i] < c:  # the alpha used here should new or old? the answer is new, why?
+        if 0 < os.alphas[i] < os.c:  # the alpha used here should new or old? the answer is new, why?
             b = bi
-        elif 0 < os.alphas[j] < c:
+        elif 0 < os.alphas[j] < os.c:
             b = bj
         else:
             b = (bi + bj) / 2
@@ -110,14 +114,13 @@ def select_j(os, i, ei):
                 max_j = k
         return max_j, max_ej
     else:
-        j = select_j_random(os, i)
-        ej = calc_e(os, j)
+        j, ej = select_j_random(os, i)
         return j, ej
 
 
 def calc_e(os, k):
     w = np.multiply(os.alphas, os.y_matrix).T * os.x_matrix  # 1*n
-    error_k = float(w * (os.x_matrix[k, :]).T - os.y_matrix[k])
+    error_k = float(w * (os.x_matrix[k, :]).T + os.b - os.y_matrix[k])
     # why not update os.e_cache immediately? because
     return error_k
 
@@ -127,7 +130,8 @@ def select_j_random(os, i):
     j = i
     while i == j:
         j = np.random.randint(os.m)
-    return j
+    ej = calc_e(os, j)
+    return j, ej
 
 
 def update_e(os, k):
@@ -146,24 +150,25 @@ def full_smo(x_matrix, y_matrix, c, tole, max_iter):
         if examine_all == 1:
             # if the entire data is not rounded through, then we choose i in order, and choose j in random
             for i in range(os.m):
-                pairs_changed += take_steps(os, i, os.c)
+                pairs_changed += take_steps(os, i)
             iters += 1
             print('examine_all, iter: %d, i: %d, pairs changed: %d' %(iters, i, pairs_changed))
         else:
             nonbound = np.nonzero((os.alphas.A > 0) * (os.alphas.A < c))[0]
             for i in nonbound:
-                pairs_changed += take_steps(os, i, os.c)
+                pairs_changed += take_steps(os, i)
             iters += 1
             print('nonbounds, iter: %d, i: %d, pairs changed: %d' % (iters, i, pairs_changed))
-        if examine_all == 1:
-            examine_all = 0
-        elif pairs_changed == 0:
-            examine_all = 1
+        # if examine_all == 1:
+        #     examine_all = 0
+        # elif pairs_changed == 0:
+        #     examine_all = 1
     return os.alphas, os.b
 
 dataArr, labelArr = import_data('testSet.txt')
-alphas, b = full_smo(np.matrix(dataArr), np.matrix(labelArr), 0.6, 0.001, 40)
+alphas, b = full_smo(np.asmatrix(dataArr), np.asmatrix(labelArr), 0.6, 0.001, 40)
 print(b)
+print (alphas[alphas>0])
 
 
 
